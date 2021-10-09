b0c7f9897d5b8738200ad9f3605ee081c4f56b7b
tgtiburon <tg.tiburon@gmail.com>
2021-10-09 11:15:57 -0400

    multiple rounds

--- assets/js/game.js
+++ assets/js/game.js
@@ -73,10 +73,19 @@ var fight = function(enemyName) {
 
 
 for(var i = 0; i <enemyNames.length; i++) {
-    //debugger;
-    var pickedEnemyName = enemyNames[i];
-    enemyHealth = 20;//should be 50
-    fight(pickedEnemyName);
+    if (playerHealth> 0){
+
+    
+        window.alert("Welcome to Robot Gladiators! Round " + (i+1));
+        //debugger;
+        var pickedEnemyName = enemyNames[i];
+        enemyHealth = 20;//should be 50
+        fight(pickedEnemyName);
+    }
+    else {
+        window.alert("You have lost your robot in battle! Game Over");
+        break;
+    }
 
 }
 
